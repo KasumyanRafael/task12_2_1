809bf28ecca1f73c4f42a1c56ac6f1ba976fc81c
студент <stud@dc.nosu>
2022-09-19 09:35:24 +0300

    Добавьте файлы проекта.

---
+++ BaseDate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task12_2_1
+{
+    class BaseDate
+    {
+        protected int day;
+        protected int month;
+        protected int year;
+
+        public BaseDate(int year,int month,int day)
+        {
+            this.year = year;
+            this.day = day;
+            this.month = month;
+        }
+
+        public virtual string GetFormat()
+        {
+            //return $"год:{year}, месяц: {month}, день: {day}";
+            return String.Format("год:{0}, месяц: {1}, день: {2}",year,month,day);
+        }
+    }
+
+    class AmericanDate:BaseDate
+    {
+        public AmericanDate(int year, int month, int day) : base(year, month, day)
+        { }
+        public override string GetFormat()
+        {
+            return $"{month}.{day}.{year}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task12_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseDate date = new BaseDate(2021, 3, 24);
+            Console.WriteLine(date.GetFormat());
+            AmericanDate date1 = new AmericanDate(2021, 3, 24);
+            Console.WriteLine(date1.GetFormat());
+        }
+    }
+}
